e961fd942b7c14cf23000c33150b749e2c0401d8
Trevor King <tgking@coastal.edu>
2024-03-11 16:54:22 -0400

    Created ViewSets

--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Vessel
+from .models import VesselSchedule
+from .models import BillOfLading
+from .models import Container
+from .serializers import VesselSerializer
+from .serializers import VesselScheduleSerializer
+from .serializers import BillOfLadingSerializer
+from .serializers import ContainerSerializer
+
+# Create your views here.
+
+class VesselViewSet(viewsets.ModelViewSet):
+    queryset = Vessel.objects.all()
+    serializer_class = VesselSerializer
+
+class VesselScheduleViewSet (viewsets.ModelViewSet):
+    queryset = VesselSchedule.objects.all()
+    serializer_class = VesselScheduleSerializer
+
+class BillOfLadingViewSet(viewsets.ModelViewSet):
+    queryset = BillOfLading.objects.all()
+    serializer_class = BillOfLadingSerializer
+
+class ContainerViewSet(viewsets.ModelViewSet):
+    queryset = Container.objects.all()
+    serializer_class = ContainerSerializer
 
 # Create your views here.
