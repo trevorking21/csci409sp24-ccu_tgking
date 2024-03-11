364981cbbf3c6cfdf7cc717e6b2ebaf84e8d0ae1
Trevor King <tgking@coastal.edu>
2024-03-11 12:52:07 -0400

    Created Models

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Vessel(models.Model):
+    vessel_name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.vessel_name
+
+class VesselSchedule(models.Model):
+    vessel = models.ForeignKey(Vessel, on_delete=models.RESTRICT)
+    voyage_number = models.CharField(max_length=50)
+    arrival_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.vessel.vessel_name} - {self.voyage_number}"
+
+class BillOfLading(models.Model):
+    voyage = models.ForeignKey(VesselSchedule, on_delete=models.RESTRICT)
+    bol_number = models.CharField(max_length=200)
+    contact_name = models.CharField(max_length=200)
+    contact_number = models.CharField(max_length=200)
+    contact_email = models.EmailField(max_length=200)
+    release_status = models.CharField(max_length=1, choices=(('C','Customs Hold' ), ('S','Steamship Hold' ), ('R','Released' ), ('A','Available for Pickup' )))
+
+    def __str__(self):
+        return self.bol_number
+
+class Container(models.Model):
+    bol = models.ForeignKey(BillOfLading, on_delete=models.RESTRICT)
+    container_number = models.CharField(max_length=200)
